0711460923cb820643da2f57caa5c73701bbf5bf
Randy Greig <randy@greigfamily.ca>
2014-01-12 19:56:08 -0700

    motor changes

--- raspibot_server.py
+++ raspibot_server.py
@@ -5,6 +5,7 @@ import socket
 import time
 import nxt.locator
 from nxt.motor import *
+from nxt.sensor import *
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -14,8 +15,9 @@ GPIO.setup(9, GPIO.OUT)
 
 brick = nxt.locator.find_one_brick()
 print "Connected to NXT..."
-m_left = Motor(brick, Port_B)
-m_right = Motor(brick, Port_C)
+m_left = Motor(brick, PORT_B)
+m_right = Motor(brick, PORT_C)
+both = nxt.SynchronizedMotors(m_left, m_right, 0)
 
 robotsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 robotsock.bind(('', 9000))
@@ -37,20 +39,26 @@ while(1):
             break
         except:
             pass
-    if sockdata == 'B':
+    if sockdata == 'M':
         print "beep"
-        brick.play_tone_and_wait(523, 500)
-        time.sleep(2)
+        brick.play_tone_and_wait(1000, 500)
+    if sockdata == 'F':
+        print "forward"
+        both.turn(100, 360, False)
+    if sockdata == 'B':
+        print "backwards"
+        both.turn(-100, 360, False)
     if sockdata == 'R':
         print "right"
-        m_right,turn(100, 360)
-        time.sleep(2)
+        m_right.turn(100, 360, False)
     if sockdata == 'L':
         print "left"
-        m_left,turn(100, 360)
-        time.sleep(2)
+        m_left.turn(100, 360, False)
     if sockdata == 'Q':
         break
+    print 'Distance:', Ultrasonic(brick, PORT_4).get_sample()
+    if Ultrasonic(brick, PORT_4).get_sample() < 20:
+        both.brake()
 
 GPIO.output(7, False)
 GPIO.output(8, False)
