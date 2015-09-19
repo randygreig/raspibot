13b9853e934ac1fea244347377a6bf061eface4e
Randy Greig <randy@greigfamily.ca>
2015-09-19 14:02:04 -0600

    2015-09-19 testing Git Shell to update

--- raspibot-control.py
+++ raspibot-control.py
@@ -5,24 +5,24 @@
 # Created:  2013-12-28
 # Author:   Randy Greig
 # Version:  0.1
-'''
-
+"""
 Dependencies:
   pygame   - http://www.pygame.org/
-'''
+"""
 ###############################################################################
 
-print "\n================================================"
-print "        raspibot Joystick control"
-print "================================================"
+print("\n================================================")
+print("        raspibot Joystick control")
+print("================================================")
 
 # Import dependent Python modules
 import socket
+
 try:
-	import pygame.joystick
+    import pygame.joystick
 except:
-	print "\nPlease install the 'pygame' module <http://www.pygame.org/>.\n"
-	quit()
+    print("\nPlease install the 'pygame' module <http://www.pygame.org/>.\n")
+    quit()
 
 robotsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 robotsock.connect(("192.168.1.50", 9000))
@@ -61,51 +61,51 @@ def handleJoyEvent(e):
                 move = round(pos * 90, 0)
                 serv = int(90 + move)
                 # and send to Arduino over serial connection
-                #servo.move(1, serv)
+                # servo.move(1, serv)
             # Y Axis
             if (axis == "Y"):
                 pos = e.dict['value']
                 move = round(pos * 90, 0)
                 serv = int(90 + move)
-                #servo.move(2, serv)
+                # servo.move(2, serv)
             # Z Axis
             if (axis == "Z"):
                 pos = e.dict['value']
                 move = round(pos * 90, 0)
                 serv = int(90 + move)
-                #servo.move(3, serv)
+                # servo.move(3, serv)
             # Throttle
             if (axis == "Throttle"):
                 pos = e.dict['value']
                 move = round(pos * 90, 0)
                 serv = int(90 + move)
-                #servo.move(4, serv)
+                # servo.move(4, serv)
 
     # Assign actions for Button DOWN events
     elif e.type == pygame.JOYBUTTONDOWN:
         # Button 1 (trigger)
         if (e.dict['button'] == 0):
-            print "Trigger Down"
+            print("Trigger Down")
             robotsock.send('M')
         # Button 2
         if (e.dict['button'] == 1):
-            print "Button 2 Down"
-	    robotsock.send('F')
+            print("Button 2 Down")
+            robotsock.send('F')
         # Button 3
         if (e.dict['button'] == 2):
-            print "Button 3 Down"
-	    robotsock.send('B')
+            print("Button 3 Down")
+            robotsock.send('B')
         # Button 4
         if (e.dict['button'] == 3):
-            print "Button 4 Down"
+            print("Button 4 Down")
             robotsock.send('L')
-	# Button 5
+        # Button 5
         if (e.dict['button'] == 4):
-            print "Button 5 Down"
-	    robotsock.send('R')
+            print("Button 5 Down")
+            robotsock.send('R')
         # Button 6
         if (e.dict['button'] == 5):
-            print "Button 6 Down"
+            print("Button 6 Down")
             robotsock.send('Q')
             quit()
 
@@ -113,31 +113,33 @@ def handleJoyEvent(e):
     elif e.type == pygame.JOYBUTTONUP:
         # Button 1 (trigger)
         if (e.dict['button'] == 0):
-            print "Trigger Up"
+            print("Trigger Up")
             # Set pin 13 LED to LOW for digital on/off demo
-            #servo.move(99, 0)
+            # servo.move(99, 0)
         # Button 2
         if (e.dict['button'] == 1):
-            print "Button 2 Up"
+            print("Button 2 Up")
         # Button 3
         if (e.dict['button'] == 2):
-            print "Button 3 Up"
+            print("Button 3 Up")
         # Button 4
         if (e.dict['button'] == 3):
-            print "Button 4 Up"
+            print("Button 4 Up")
         # Button 5
         if (e.dict['button'] == 4):
-            print "Button 5 Up"
+            print("Button 5 Up")
         # Button 6
         if (e.dict['button'] == 5):
-            print "Button 6 Up"
+            print("Button 6 Up")
 
     else:
         pass
 
+
 # Print the joystick position
 def output(line, stick):
-    print "Joystick: %d; %s" % (stick, line)
+    print("Joystick: %d; %s" % (stick, line))
+
 
 # Wait for joystick input
 def joystickControl():
@@ -146,21 +148,22 @@ def joystickControl():
         if (e.type == pygame.JOYAXISMOTION or e.type == pygame.JOYBUTTONDOWN or e.type == pygame.JOYBUTTONUP):
             handleJoyEvent(e)
 
+
 # Main method
 def main():
     # Initialize pygame
     pygame.joystick.init()
     pygame.display.init()
     if not pygame.joystick.get_count():
-        print "\nPlease connect a joystick and run again.\n"
+        print("\nPlease connect a joystick and run again.\n")
         quit()
-    print "\n%d joystick(s) detected." % pygame.joystick.get_count()
+    print("\n%d joystick(s) detected." % pygame.joystick.get_count())
     for i in range(pygame.joystick.get_count()):
         myjoy = pygame.joystick.Joystick(i)
         myjoy.init()
         joy.append(myjoy)
-        print "Joystick %d: " % (i) + joy[i].get_name()
-    print "Depress joystick button 6 to quit.\n"
+        print("Joystick %d: " % (i) + joy[i].get_name())
+    print("Depress joystick button 6 to quit.\n")
 
     # Run joystick listener loop
     joystickControl()
